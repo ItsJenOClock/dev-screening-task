895a6ed6b9098f3841cb45d51834d76aa047610d
Jen Nguyen <26128269+ItsJenOClock@users.noreply.github.com>
2024-08-08 01:54:50 -0400

    Updated view for smaller screen sizes, updated comments

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
-// set up for DOM content loaded event
+// Set up for DOM content loaded event
 addEventListener('DOMContentLoaded', () => {
-    // declare const variables from ID card and icon element
+    // Declare const variables from ID card and icon element
     const idCard = document.querySelector('.id-card');
     const editIcon = document.querySelector('.edit-icon');
     const saveIcon = document.querySelector('.save-icon');
 
     function toggleEditMode() {
-        // check if ID card has edit-mode class, hide save icon and display edit icon.
-        // if not,
+        // Check if ID card has edit-mode class, hide save icon and display edit icon
+        // If not, then hide edit icon and display save icon
         if (idCard.classList.contains('edit-mode')) {
             idCard.classList.remove('edit-mode'); // remove ID card from edit mode
             saveIcon.style.display = 'none'; // remove save icon
@@ -20,7 +20,7 @@ addEventListener('DOMContentLoaded', () => {
         }
     }
 
-    // call toggleEditMode function when icons are clicked
+    // Call toggleEditMode function when icons are clicked
     editIcon.addEventListener('click', toggleEditMode);
     saveIcon.addEventListener('click', toggleEditMode);
 });
